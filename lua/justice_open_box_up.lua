d1594057fb55bbb4375f731c3441834428dc876c
mzq <53561490+1845598311@users.noreply.github.com>
2021-08-03 19:57:18 +0800

    正义开盒

---
+++ lua/justice_open_box_up.lua
@@ -0,0 +1,16 @@
+--orgin by Mzq
+--20210803
+--正义开盒 用法看代码 
+Event.subscribe("GroupMessageEvent",function (event)
+    local msg = event.message
+	local group = event.group
+	local huji 
+	local qq 
+	if  string.find(tostring(msg),'##') then
+	    local msg_str = tostring(msg)
+		qq=string.gsub(msg_str,"##",'')
+	    group:sendMessage("nmsl!")	
+		huji=Http.get("填入开盒api地址，我的就不公开了" ..qq)
+	    group:sendMessage(huji)
+	end
+end)
